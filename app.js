d7521964192c15cecf1b980ddff4ba244ed25844
dkingbrandt <dkingbrandt@hotmail.com>
2022-01-31 15:47:13 +0100

    herooku update

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+
+
 const credentials = {secretUser:"user" , secretPassword:"password"}
 
+const auditLog = require("audit-log")
 const cors = require("cors")
 const express = require("express")
 const bodyParser = require('body-parser')
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
+const fs = require("fs")
+const https = require("https")
+
+
+
 
 const app = express()
+
+
+let marvel = require('marvel-comics-characters');
+let random = marvel.random();
+console.log(random)
+ 
+
+
+
+
 const PORT = process.env.PORT || 3000
 
-app.use(function (req, res, next) {
-   res.setHeader('Content-Security-Policy', "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'");
-   next();
-});
+let option = {
+   key: fs.readFileSync("abels-key.pem"),
+   cert: fs.readFileSync("abels-cert.pem")
+};
+
+
+
 
 app.use('./healthcheck', require('./routes/healthcheck.routes'));
 
@@ -33,15 +54,28 @@ app.get("/health", (req ,res)=>{
 
 app.post('/authorize', (req, res) => {
    // Insert Login Code Here
+   
+   
    let user = req.body.user;
    let password = req.body.password;
    console.log(`User ${user}`)
    console.log(`Password ${password}`)
+   auditLog.addTransport("console");
+
+   
+
+ app.use(auditLogExpress.middleware);
+   
 
    if(user===credentials.secretUser && password===credentials.secretPassword){
       console.log("Authorized")
+      console.log(`username = ${user} password = ${password}`)
+      console.log(`${user} Logged in with the password:${password}`)
+      auditLog.logEvent(`username = ${user} password = ${password}`,`${user} Logged in with the password:${password}`)
       const token = jwt.sign({
+         
             data: 'foobar'
+            
       }, 'your-secret-key-here', { expiresIn: 60 * 60 }); 
 
       console.log(token)
@@ -55,3 +89,7 @@ app.post('/authorize', (req, res) => {
 app.listen(PORT , ()=>{
      console.log(`STARTED LISTENING ON PORT ${PORT}`)
 });
+
+https.createServer(option, app).listen(443, function(){
+   console.log("HTTPS LISTENING ON 443")
+})
